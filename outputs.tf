c88f2a2e5a78557a1896a5c60a88619535dc6949
Jeroen Rijks <jeroen@getorchestra.io>
2025-12-05 18:10:57 +0000

    Expose only required outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,10 @@
 output "orchestra_compute_resource_inputs" {
   value = <<-EOT
 Enter the following values in the Orchestra UI:
-- Tenant ID: ${data.azurerm_client_config.current.tenant_id}
-- Subscription ID: ${data.azurerm_subscription.current.subscription_id}
-- Client ID: ${azuread_application.this.client_id}
-- Unique Identifier: ${var.name_prefix}-compute-${local.suffix}
+- Compute resource identifier: ${var.name_prefix}-compute-${local.suffix}
 - Resource group name: ${var.resource_group_name}
-- Container app environment id: ${local.container_app_environment_id}
+- Azure tenant ID: ${data.azurerm_client_config.current.tenant_id}
+- Azure client ID: ${azuread_application.this.client_id}
+- Azure subscription ID: ${data.azurerm_subscription.current.subscription_id}
 EOT
 }
