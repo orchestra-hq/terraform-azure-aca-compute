7422fe2a8d6af463edf8b5e717f210a5ba882606
Jeroen Rijks <jeroen@getorchestra.io>
2026-02-11 15:49:08 +0000

    Use existing app registration and enterprise app, if set

--- locals.tf
+++ locals.tf
@@ -1,10 +1,12 @@
 locals {
   suffix                           = random_id.random_suffix.hex
-  federated_credentials_issuer     = "https://cognito-identity.amazonaws.com"
   create_container_app_environment = var.container_app_environment_name == null
   location                         = local.create_container_app_environment ? data.azurerm_resource_group.this.location : data.azurerm_container_app_environment.this[0].location
   container_app_environment_id     = local.create_container_app_environment ? azurerm_container_app_environment.this[0].id : data.azurerm_container_app_environment.this[0].id
-
+  normalized_secret_names = {
+    for key, value in var.container_app_job_secret_env_vars :
+    key => replace(lower(key), "_", "-")
+  }
   tags = merge(
     {
       Application = "Orchestra Technologies"
@@ -13,10 +15,9 @@ locals {
     var.tags,
   )
 
-  normalized_secret_names = {
-    for key, value in var.container_app_job_secret_env_vars :
-    key => replace(lower(key), "_", "-")
-  }
+  federated_credentials_issuer = "https://cognito-identity.amazonaws.com"
+  service_principal_object_id  = var.enterprise_app_object_id == "" ? azuread_service_principal.this[0].object_id : var.enterprise_app_object_id
+  azure_client_id_output_value = var.enterprise_app_object_id == "" ? azuread_application.this[0].client_id : "Set the Client ID of the Enterprise Application that was manually created"
 }
 
 resource "random_id" "random_suffix" {
--- orchestra_permissions.tf
+++ orchestra_permissions.tf
@@ -1,17 +1,24 @@
+# Maps to the App Registration
 resource "azuread_application" "this" {
+  count = var.enterprise_app_object_id == "" ? 1 : 0
+
   display_name = "${var.name_prefix}-app-registration-${local.suffix}"
 }
 
+# Maps to the Enterprise Application
 resource "azuread_service_principal" "this" {
-  client_id = azuread_application.this.client_id
+  count = var.enterprise_app_object_id == "" ? 1 : 0
 
+  client_id = azuread_application.this[0].client_id
   feature_tags {
     enterprise = true
   }
 }
 
 resource "azuread_application_federated_identity_credential" "this" {
-  application_id = azuread_application.this.id
+  count = var.enterprise_app_object_id == "" ? 1 : 0
+
+  application_id = azuread_application.this[0].id
   display_name   = "${var.name_prefix}-federated-credential-${local.suffix}"
   description    = "Federated credentials used to grant Orchestra access to this account's Azure resources for hybrid compute"
 
@@ -23,23 +30,23 @@ resource "azuread_application_federated_identity_credential" "this" {
 resource "azurerm_role_assignment" "container_app_job_resource_group" {
   scope                = data.azurerm_resource_group.this.id
   role_definition_name = "Container Apps Jobs Contributor"
-  principal_id         = azuread_service_principal.this.object_id
+  principal_id         = local.service_principal_object_id
 }
 
 resource "azurerm_role_assignment" "secrets_management" {
   scope                = azurerm_key_vault.this.id
   role_definition_name = "Key Vault Crypto Officer"
-  principal_id         = azuread_service_principal.this.object_id
+  principal_id         = local.service_principal_object_id
 }
 
 resource "azurerm_role_assignment" "storage_blob_data" {
   scope                = azurerm_storage_account.this.id
   role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azuread_service_principal.this.object_id
+  principal_id         = local.service_principal_object_id
 }
 
 resource "azurerm_role_assignment" "log_analytics_contributor" {
   scope                = data.azurerm_resource_group.this.id
   role_definition_name = "Log Analytics Reader"
-  principal_id         = azuread_service_principal.this.object_id
+  principal_id         = local.service_principal_object_id
 }
